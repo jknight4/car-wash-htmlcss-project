798ccedae7933102e1761bc54592a4bd5ef048ed
knightj <noreply>
2022-03-18 18:49:11 -0400

    more js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 // Set current year
 const yearEl = document.querySelector(".copyright-date");
 const currentYear = new Date().getFullYear();
@@ -14,3 +12,27 @@ btnNavEl.addEventListener('click', function() {
     headerEl.classList.toggle('nav-open');
 }) ;
 
+// Smooth scrolling animation
+/// for the browsers that can't use scroll-behavior
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function(link){
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+        const href = link.getAttribute('href');
+
+        // Scroll back to top
+        if (href === "#") window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+        // Scroll to other links
+        if (href !== '#' && href.startsWith('#')){
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({behavior:"smooth"});
+        }
+        // Close mobile nav
+        if(link.classList.contains('main-nav-link'))
+            headerEl.classList.toggle("nav-open");
+
+    });
+});
