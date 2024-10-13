1fe38f1379756ba39579f9e686772c93cd61b625
Jeremy <knightj0044@gmail.com>
2024-10-12 22:32:14 -0500

    multi step form

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ yearEl.textContent = currentYear;
 
 
 // Make mobile navigation work
-const btnNavEl  = document.querySelector(".btn-mobile-nav");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener('click', function() {
+btnNavEl.addEventListener('click', function () {
     headerEl.classList.toggle('nav-open');
-}) ;
+});
 
 // Smooth scrolling animation
 /// for the browsers that can't use scroll-behavior
 const allLinks = document.querySelectorAll("a:link");
-allLinks.forEach(function(link){
-    link.addEventListener('click', function(e){
+allLinks.forEach(function (link) {
+    link.addEventListener('click', function (e) {
         e.preventDefault();
         const href = link.getAttribute('href');
 
@@ -26,12 +26,12 @@ allLinks.forEach(function(link){
             behavior: 'smooth',
         });
         // Scroll to other links
-        if (href !== '#' && href.startsWith('#')){
+        if (href !== '#' && href.startsWith('#')) {
             const sectionEl = document.querySelector(href);
-            sectionEl.scrollIntoView({behavior:"smooth"});
+            sectionEl.scrollIntoView({ behavior: "smooth" });
         }
         // Close mobile nav
-        if(link.classList.contains('main-nav-link'))
+        if (link.classList.contains('main-nav-link'))
             headerEl.classList.toggle("nav-open");
 
     });
@@ -39,14 +39,14 @@ allLinks.forEach(function(link){
 
 // Sticky navigation
 const sectionHeroEl = document.querySelector(".section-hero");
-const obs = new IntersectionObserver(function(entries){
-    const ent = entries[0]; 
+const obs = new IntersectionObserver(function (entries) {
+    const ent = entries[0];
     console.log(ent);
-    if(ent.isIntersecting === false){
+    if (ent.isIntersecting === false) {
         document.body.classList.add('sticky');
     }
 
-    if(ent.isIntersecting){
+    if (ent.isIntersecting) {
         document.body.classList.remove('sticky');
     }
 }, {
@@ -62,8 +62,60 @@ obs.observe(sectionHeroEl);
 
 const accordion = document.getElementsByClassName("item");
 
-for (i=0; i<accordion.length; i++ ) {
-    accordion[i].addEventListener('click', function(){
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function () {
         this.classList.toggle('active-content');
     });
-    };
+};
+
+
+// Form
+
+const multiStepForm = document.querySelector("[data-multi-step");
+const formScreens = [...multiStepForm.querySelectorAll("[data-step]")];
+
+let currentStep = formScreens.findIndex((step) => {
+    return step.classList.contains("active")
+});
+
+if (currentStep < 0) {
+    currentStep = 0;
+    showCurrentStep();
+}
+
+multiStepForm.addEventListener("click", (e) => {
+    let incrementor;
+    if (e.target.matches("[data-next]")) {
+        incrementor = 1;
+    } else if (e.target.matches("[data-back]")) {
+        incrementor = -1
+    }
+
+    if (incrementor == null) return;
+
+    const inputs = [...formScreens[currentStep].querySelectorAll("input")];
+    const allValid = inputs.every(input => input.reportValidity());
+
+    console.log(allValid);
+
+    if (allValid) {
+        currentStep += incrementor;
+        showCurrentStep();
+    }
+
+    console.log(currentStep);
+});
+
+
+formScreens.forEach(step => {
+    step.addEventListener("animationend", e => {
+        formScreens[currentStep].classList.remove("hide");
+        e.target.classList.toggle("hide", !e.target.classList.contains("active"))
+    })
+})
+
+function showCurrentStep() {
+    formScreens.forEach((step, index) => {
+        step.classList.toggle("active", index === currentStep);
+    })
+}
