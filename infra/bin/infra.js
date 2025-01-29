45222d0864da35b64e0a9baf4c6919864b020c29
Jeremy <knightj0044@gmail.com>
2025-01-29 07:47:01 -0600

    test

--- infra/bin/infra.js
+++ infra/bin/infra.js
@@ -13,12 +13,12 @@ const app = new cdk.App();
 //   },
 // });
 
-// new CICDStack(app, "CICDStack", {
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//     region: process.env.CDK_DEFAULT_REGION,
-//   },
-// });
+new CICDStack(app, "CICDStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
 
 new AppStage(app, "Prod", {
   env: {
