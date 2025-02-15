05333c9636d7dda39c983a9329a64a3dfd840725
Jeremy <knightj0044@gmail.com>
2025-02-15 11:33:14 -0600

    test

--- infra/lib/form-service-stack.js
+++ infra/lib/form-service-stack.js
@@ -37,7 +37,7 @@ class FormServiceStack extends Stack {
 
     const httpApi = new HttpApi(this, "FormServiceHttpApi", {
       corsPreflight: {
-        allowMethods: [CorsHttpMethod.PUT],
+        allowMethods: [CorsHttpMethod.PUT, CorsHttpMethod.OPTIONS],
         allowHeaders: ["Content-Type", "Authroization"],
       },
       allowOrigins: ["https://primetimeauto.knightj.xyz"],
